cc3cc7a22918cbf9f4439e9817a7b2858d00beae
raphtis <angelvelazquez20@gmail.com>
2022-04-27 23:35:53 -0500

    Updated API call

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [ data, setData ] = useState({})
   const [ location, setLocation ] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=aaa77aec8939b09a0ca583eca113379c`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=aaa77aec8939b09a0ca583eca113379c&units=imperial`;
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -20,7 +20,9 @@ function App() {
   }
   return (
     <div className="app">
+      
       <div className="search">
+      <h1>Weather</h1>
         <input 
           value={location}
           onChange={event => setLocation(event.target.value)}
@@ -35,26 +37,30 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ?  <h1>{data.main.temp}°F</h1> : null}
+            {data.main ?  <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="desc">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+
+        {data.name !== undefined &&
         <div className="bottom">
           <div className="feelsLike">
-          {data.main ?  <p className='bold'>{data.main.feels_like}°F</p> : null}
+          {data.main ?  <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
             <p>Feels Like</p>
           </div>
           <div className="humidity">
-            {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className='bold'>12mph</p>
+            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
             <p>Wind Speed</p>
           </div>
         </div>
+        }
+
       </div>
     </div>
   );
